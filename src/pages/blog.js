1aa192e617d6e8e1df576e98a9f3dd226be64c1f
Si Truong <si.truongtan@gmail.com>
2021-10-27 11:19:34 +0700

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,30 +3,32 @@ import Layout from "../components/layout";
 
 import { graphql } from "gatsby";
 
+import { MDXRenderer } from "gatsby-plugin-mdx";
+
 const BlogPage = ({ data }) => {
   // console.log(data);
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
-export const querytr7 = graphql`
+export const query = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-        siteUrl
-        description
-      }
-    }
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
